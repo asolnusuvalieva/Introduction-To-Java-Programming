1340fd657c2f73e2faa13a5806a268ac3d70c8d7
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-02 22:45:40 +0600

    Do Listing 9.1

---
+++ Chapter9/src/TestSimpleCircle.java
@@ -0,0 +1,47 @@
+class Circle {
+    /** The radius of this circle */
+    double radius = 1;
+
+    /** Construct a circle object */
+    Circle() { }
+
+    /** Construct a circle object */
+    Circle(double newRadius) {
+        radius = newRadius;
+    }
+    /** Return the area of this circle */
+    double getArea() {
+        return radius * radius * Math.PI;
+    }
+    /** Return the perimeter of this circle */
+    double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+    /** Set new radius for this circle */
+     void setRadius(double newRadius) {
+        radius = newRadius;
+    }
+}
+public class TestSimpleCircle {
+    public static void main(String[] args) {
+        // Create a circle with radius 1
+        Circle circle1 = new Circle();
+        System.out.println("The area of the circle of radius "
+                + circle1.radius + " is " + circle1.getArea());
+
+        // Create a circle with radius 25
+        Circle circle2 = new Circle(25);
+        System.out.println("The area of the circle of radius "
+                + circle2.radius + " is " + circle2.getArea());
+
+        // Create a circle with radius 125
+        Circle circle3 = new Circle(125);
+        System.out.println("The area of the circle of radius "
+                + circle3.radius + " is " + circle3.getArea());
+
+        // Modify circle radius
+        circle2.radius = 100; // or circle2.setRadius(100)
+        System.out.println("The area of the circle of radius "
+                + circle2.radius + " is " + circle2.getArea());
+    }
+}
