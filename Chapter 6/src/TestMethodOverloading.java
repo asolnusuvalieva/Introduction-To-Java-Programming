001fec2df5eeef37200cc49d920d2a0e04b3174f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-09 23:23:52 +0600

    Implement user output in Listing 6.9

--- Chapter 6/src/TestMethodOverloading.java
+++ Chapter 6/src/TestMethodOverloading.java
@@ -1,4 +1,12 @@
 public class TestMethodOverloading {
+    public static void main(String[] args) {
+        // Invoke the max method with int parameters
+        System.out.println("The maximum of 3 and 4 is " + max(3, 4));
+        // Invoke the max method with the double parameters
+        System.out.println("The maximum of 3.0 and 5.4 is " + max(3.0, 5.4));
+        // Invoke the max method with three double parameters
+        System.out.println("The maximum of 3.0, 5.4, and 10.14 is " + max(3.0, 5.4, 10.14));
+    }
     public static int max(int num1, int num2) {
         if (num1 > num2)
             return num1;
