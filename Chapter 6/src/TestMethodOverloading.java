8497c32b3b767264a8fa308f179c0e463061495c
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-09 23:23:05 +0600

    Implement max(double num1, double num2, double num3) in Listing 6.9

--- Chapter 6/src/TestMethodOverloading.java
+++ Chapter 6/src/TestMethodOverloading.java
@@ -12,4 +12,8 @@ public class TestMethodOverloading {
         else
             return num2;
     }
+
+    public static double max(double num1, double num2, double num3){
+        return max(max(num1, num2), num3);
+    }
 }
