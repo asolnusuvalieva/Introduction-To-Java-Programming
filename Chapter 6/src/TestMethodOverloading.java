6e56ffb5d0f3c029e770c5ae78683ac4f66ae33c
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-09 23:21:52 +0600

    Implement max(double num1, double num2) in Listing 6.9

--- Chapter 6/src/TestMethodOverloading.java
+++ Chapter 6/src/TestMethodOverloading.java
@@ -1,6 +1,14 @@
 public class TestMethodOverloading {
     public static int max(int num1, int num2) {
-        if (num1 > num2) return num1;
+        if (num1 > num2)
+            return num1;
+        else
+            return num2;
+    }
+
+    public static double max(double num1, double num2) {
+        if (num1 > num2)
+            return num1;
         else
             return num2;
     }
