74b3548f71e10f973467e43fa4331bddb4ed909e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-11 23:14:25 +0600

    Implement user output

---
+++ Chapter 7/src/AnalyzeNumbers.java
@@ -0,0 +1,10 @@
+public class AnalyzeNumbers {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.print("Enter the number of items: ");
+
+        int n = input.nextInt();
+        double [] numbers = new double[n];
+        double sum = 0;
+    }
+}
