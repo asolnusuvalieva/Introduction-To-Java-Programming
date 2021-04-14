212afd2e51d497f0b92bb61b3e88d015a9181998
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-14 23:55:58 +0600

    Implement swap(int n1, int n2) in Listing 7.2

---
+++ Chapter 7/src/TestPassArray.java
@@ -0,0 +1,12 @@
+public class TestPassArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2};
+    }
+
+    public static void swap(int n1, int n2) {
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+}
+
