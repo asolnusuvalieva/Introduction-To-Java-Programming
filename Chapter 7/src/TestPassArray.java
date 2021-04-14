6c1d4fec16424e5c9bd9b3e574cf3598ac5059d4
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-14 23:57:34 +0600

    Implement user output information before invoking swap method in Listing 7.2

--- Chapter 7/src/TestPassArray.java
+++ Chapter 7/src/TestPassArray.java
@@ -1,6 +1,13 @@
 public class TestPassArray {
     public static void main(String[] args) {
         int[] a = {1, 2};
+
+        // Swap elements using the swap method
+        System.out.println("Before invoking swap");
+        System.out.println("array is {" + a[0] + ", " + a[1] + "}"); swap(a[0], a[1]);
+        System.out.println("After invoking swap");
+        System.out.println("array is {" + a[0] + ", " + a[1] + "}");
+
     }
 
     public static void swap(int n1, int n2) {
