4ba43d3dfe34d3fcac46972c8741f2bdca7fa0bd
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-14 23:56:40 +0600

    Implement swapFirstTwoInArray(int[] array) in Listing 7.2

--- Chapter 7/src/TestPassArray.java
+++ Chapter 7/src/TestPassArray.java
@@ -8,5 +8,11 @@ public class TestPassArray {
         n1 = n2;
         n2 = temp;
     }
+
+    public static void swapFirstTwoInArray(int[] array) {
+        int temp = array[0];
+        array[0] = array[1];
+        array[1] = temp;
+    }
 }
 
