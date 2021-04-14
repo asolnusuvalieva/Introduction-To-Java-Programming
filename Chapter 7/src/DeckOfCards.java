769c14f237a03a9e0b14a1f85e23f51d6d63184b
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-14 23:52:10 +0600

    Initialize the cards in Listing 7.2

---
+++ Chapter 7/src/DeckOfCards.java
@@ -0,0 +1,12 @@
+public class DeckOfCards {
+    public static void main(String[] args) {
+        int[] deck = new int[52];
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+
+        //initialize the cards
+        for(int i = 0; i < 52; i++){
+            deck[i] = i;
+        } //{0, 1, 2, 3, ..., 51}
+    }
+}
