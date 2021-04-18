4a0433c09c014b9528e9aadb0d885146b0d50282
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-18 23:01:27 +0600

    Finish createArray() method of Listing 7.4

--- Chapter 7/src/CountLettersInArray.java
+++ Chapter 7/src/CountLettersInArray.java
@@ -2,6 +2,12 @@ public class CountLettersInArray {
     public static char[] createArray() {
         // Declare an array of characters and create it
         char[] chars = new char[100];
+
+        // Create lowercase letters randomly and assign
+        // them to the array
+        for (int i = 0; i < chars.length; i++)
+            chars[i] = RandomCharacter.getRandomLowerCaseLetter();
+
         return chars;
     }
 }
