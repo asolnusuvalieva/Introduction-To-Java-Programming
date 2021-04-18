c59b030c2ea7ee91d87a3754fa56ccccd389583c
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-18 23:09:13 +0600

    Create the main method in Listing 7.4

--- Chapter 7/src/CountLettersInArray.java
+++ Chapter 7/src/CountLettersInArray.java
@@ -1,4 +1,7 @@
 public class CountLettersInArray {
+    public static void main(String[] args) {
+
+    }
     public static char[] createArray() {
         // Declare an array of characters and create it
         char[] chars = new char[100];
