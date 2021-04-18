c40aef12051b7d3fa17d0e8f63c0859306ecac91
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-18 23:04:19 +0600

    Finish displayArray(char[] chars) method in Listing 7.4

--- Chapter 7/src/CountLettersInArray.java
+++ Chapter 7/src/CountLettersInArray.java
@@ -12,6 +12,11 @@ public class CountLettersInArray {
     }
 
     public static void displayArray(char[] chars) {
-
+        for (int i = 0; i < chars.length; i++){
+             if ((i + 1) % 20 == 0)
+                System.out.println(chars[i]);
+             else
+                System.out.print(chars[i] + " ");
+        }
     }
 }
