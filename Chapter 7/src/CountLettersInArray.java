64f8b2a9fec1069fc5d23e3d46f62519e4d42ce8
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-18 23:00:01 +0600

    Create an array in createArray() method of Listing 7.4

---
+++ Chapter 7/src/CountLettersInArray.java
@@ -0,0 +1,7 @@
+public class CountLettersInArray {
+    public static char[] createArray() {
+        // Declare an array of characters and create it
+        char[] chars = new char[100];
+        return chars;
+    }
+}
