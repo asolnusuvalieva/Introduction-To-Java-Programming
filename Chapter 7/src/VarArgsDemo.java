ca54256a3699bfa8226cac9aa527a1f80114e378
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-23 23:49:40 +0600

    Implement method printMax(double... numbers)

--- Chapter 7/src/VarArgsDemo.java
+++ Chapter 7/src/VarArgsDemo.java
@@ -4,5 +4,13 @@ public class VarArgsDemo {
             System.out.println("No argument passed");
             return;
         }
+        double result = numbers[0];
+
+
+        for (int i = 1; i < numbers.length; i++)
+            if (numbers[i] > result)
+                result = numbers[i];
+
+        System.out.println("The max value is " + result);
     }
 }
