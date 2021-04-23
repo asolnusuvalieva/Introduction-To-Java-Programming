593c3869c058c9673a0a176da592008b50e6d286
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-23 23:58:14 +0600

    Finish Listing 7.8

--- Chapter 7/src/SelectionSort.java
+++ Chapter 7/src/SelectionSort.java
@@ -1,5 +1,20 @@
 public class SelectionSort {
     public static void selectionSort(int[] list){
+        for(int i = 0; i < list.length - 1; i++){
+            int currentMin = list[i];
+            int currentMinIndex = i;
 
+            for(int j = i + 1; j < list.length; j++){
+                if(list[j] < currentMin){
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if(currentMinIndex != i){
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
     }
 }
