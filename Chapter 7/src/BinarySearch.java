1c29ed9d6d88d6552cac0bc74e640a65c1fac517
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-23 23:54:55 +0600

    Start the binarySearch(int[] list, int key) method

---
+++ Chapter 7/src/BinarySearch.java
@@ -0,0 +1,8 @@
+public class BinarySearch {
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+
+        return -low - 1;
+    }
+}
