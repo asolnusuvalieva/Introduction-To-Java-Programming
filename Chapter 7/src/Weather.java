c521e8658aa345a1819afa7d11f0124613ccaa31
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-24 23:08:13 +0600

    Add initialization code

--- Chapter 7/src/Weather.java
+++ Chapter 7/src/Weather.java
@@ -7,5 +7,17 @@ public class Weather {
 
         double[][][] data = new double[NUMBER_OF_DAYS][NUMBER_OF_HOURS][2];
         Scanner scanner = new Scanner(System.in);
+
+        //initialization
+        for(int k = 0; k < NUMBER_OF_DAYS*NUMBER_OF_HOURS; k++){
+            int day = scanner.nextInt();
+            int hour = scanner.nextInt();
+            double temperature = scanner.nextDouble();
+            double humidity = scanner.nextDouble();
+
+            data[day - 1][hour - 1][0] = temperature;
+            data[day - 1][hour - 1][1] = humidity;
+        }
+
     }
 }
