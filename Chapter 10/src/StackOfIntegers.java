4158cb6419c45fb49ef6901aacda9c31c83c537f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-25 22:18:47 +0600

    Add two constructors

--- Chapter 10/src/StackOfIntegers.java
+++ Chapter 10/src/StackOfIntegers.java
@@ -3,4 +3,13 @@ public class StackOfIntegers {
     private int size;
     public static final int DEFAULT_CAPACITY = 16;
 
+    /** Construct a stack with the default capacity 16 */
+    public StackOfIntegers() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /** Construct a stack with the specified maximum capacity */
+    public StackOfIntegers(int capacity) {
+        elements = new int[capacity];
+    }
 }
