54bdc6e5e8aba19ab78746ddfd938cfa77518ad1
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-04-25 20:38:26 +0600

    Add data fields and two constructors

---
+++ Chapter 10/src/BMI.java
@@ -0,0 +1,19 @@
+public class BMI {
+    private String name;
+    private int age;
+    private double weight; // in pounds
+    private double height; // in inches
+    public static final double KILOGRAMS_PER_POUND = 0.45359237;
+    public static final double METERS_PER_INCH = 0.0254;
+
+    public BMI(String name, int age, double weight, double height) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public BMI(String name, double weight, double height) {
+        this(name, 20, weight, height);
+    }
+}
