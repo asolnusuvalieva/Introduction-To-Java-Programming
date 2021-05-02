07901a635a8095c6dffdf6a6e15fc631cfdb791e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-02 22:58:20 +0600

    Finish ShortExamples Ex

---
+++ Old Book Exercises/src/ShortExamples.java
@@ -0,0 +1,15 @@
+public class ShortExamples {
+    public static void main(String[] args) {
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 0; i < 10; ++i) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+        for(int i = 0; i < 10; i++){
+            sum2 += i;
+        }
+        System.out.println(sum2);
+    }
+}
