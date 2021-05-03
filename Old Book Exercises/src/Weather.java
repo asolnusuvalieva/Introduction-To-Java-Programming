3519d2fa0dd390caee0357e807f03baa6c2e6ba4
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-03 23:49:45 +0600

    Add data fields

---
+++ Old Book Exercises/src/Weather.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Weather {
+    public static void main(String[] args) {
+        final int NUMBER_OF_DAYS = 10;
+        final int NUMBER_OF_HOURS = 24;
+
+        double[][][] data = new double[NUMBER_OF_DAYS][NUMBER_OF_HOURS][2];
+        Scanner scanner = new Scanner(System.in);
+    }
+}
