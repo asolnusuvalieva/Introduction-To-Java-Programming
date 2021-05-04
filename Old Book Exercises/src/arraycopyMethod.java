baff6c9cbebd81131f12c4fe048f2533aa4c3ab9
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-04 23:50:25 +0600

    User output

--- Old Book Exercises/src/arraycopyMethod.java
+++ Old Book Exercises/src/arraycopyMethod.java
@@ -2,5 +2,12 @@ public class arraycopyMethod {
     public static void main(String[] args) {
         int [] sourceArray = {1, 2, 3, 4, 5};
         int [] targetArray = new int[sourceArray.length];
+
+        System.arraycopy(sourceArray, 0, targetArray, 0, sourceArray.length);
+
+        System.out.println("The contents of the source Array: ");
+        for(int i = 0; i < sourceArray.length; i++){
+            System.out.print(sourceArray[i] + " ");
+        }
     }
 }
