d8165b971ca27b02e176dad3e8ce5c2fb36f351d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-04 23:52:24 +0600

    Add the data fields

--- Old Book Exercises/src/DeckOfCards.java
+++ Old Book Exercises/src/DeckOfCards.java
@@ -1,5 +1,7 @@
 public class DeckOfCards {
     public static void main(String[] args) {
-
+        int[] deck = new int[52];
+        String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+        String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
     }
 }
