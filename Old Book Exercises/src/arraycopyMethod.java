7ca24e59ffa9d4e4704c1ba782f28d14ab2f326b
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-04 23:49:57 +0600

    Add data fields

--- Old Book Exercises/src/arraycopyMethod.java
+++ Old Book Exercises/src/arraycopyMethod.java
@@ -1,5 +1,6 @@
 public class arraycopyMethod {
     public static void main(String[] args) {
-
+        int [] sourceArray = {1, 2, 3, 4, 5};
+        int [] targetArray = new int[sourceArray.length];
     }
 }
