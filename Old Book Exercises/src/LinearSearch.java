a1736d0a8188e57a57a0cb463d66964b052f8494
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-05 23:33:04 +0600

    Implement linearSearch() method

--- Old Book Exercises/src/LinearSearch.java
+++ Old Book Exercises/src/LinearSearch.java
@@ -1,5 +1,10 @@
 public class LinearSearch {
-    public static void main(String[] args) {
-
+    public static int linearSearch(int[] array, int keyElement){
+        for(int i = 0; i < array.length; i++){
+            if(array[i] == keyElement){
+                return i;
+            }
+        }
+        return -1;
     }
 }
