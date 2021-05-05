e394f184c2235ca35c69ae597ad50eec6d38dfc2
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-05 23:34:16 +0600

    Add info (comments)

---
+++ Old Book Exercises/src/CountTheOccurrenceOfEachLetter.java
@@ -0,0 +1,9 @@
+//A program to count the occurrences of each letter in an array of characters.
+/*
+1. Randomly Generate 100 lowercase letters and assign into an array of chars
+2. Create an array of 26 int values each of which counts the occurrences of a letter,
+counts[0] counts the number of a’s, counts[1] counts the number of b’s, and so on.
+ */
+
+public class CountTheOccurrenceOfEachLetter {
+}
