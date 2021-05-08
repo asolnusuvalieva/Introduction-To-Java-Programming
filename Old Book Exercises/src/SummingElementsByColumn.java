136f0cf375a1f323499c354eb4dfe78ca64ae630
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-08 23:32:31 +0600

    Add the data fields

--- Old Book Exercises/src/SummingElementsByColumn.java
+++ Old Book Exercises/src/SummingElementsByColumn.java
@@ -1,5 +1,9 @@
 public class SummingElementsByColumn {
     public static void main(String[] args) {
-
+        int[][] example = {
+                {2, 3, 4},
+                {4, 5, 6},
+                {5, 5, 6}
+        };
     }
 }
