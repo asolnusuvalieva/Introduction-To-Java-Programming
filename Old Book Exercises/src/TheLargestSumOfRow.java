50f2f9731b75d7d9eeb59d70b6af1ce12ebf3c3e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-08 23:29:14 +0600

    Implement the logic of the method

--- Old Book Exercises/src/TheLargestSumOfRow.java
+++ Old Book Exercises/src/TheLargestSumOfRow.java
@@ -7,7 +7,23 @@ public class TheLargestSumOfRow {
 
     }
     public static void whichRowHasTheLargestSum(int[][] list){
+        int maxRowIndex = 0;
+        int maxSum = 0;
 
+        for(int j = 0; j < list[0].length; j++){
+            maxSum += list[0][j];
+        }
+
+        for(int i = 1; i < list.length; i++){
+            int currentSum = 0;
+            for(int j = 0; j < list[i].length; j++){
+                currentSum += list[i][j];
+            }
+            if(currentSum > maxSum){
+                maxSum = currentSum;
+                maxRowIndex = i;
+            }
+        }
     }
 
 }
