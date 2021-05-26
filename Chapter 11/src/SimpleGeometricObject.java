5795fe34508e9b6e66a75bcaf0088d0cb459a81c
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-26 22:22:35 +0600

    Add a constructor that accept no arguments

--- Chapter 11/src/SimpleGeometricObject.java
+++ Chapter 11/src/SimpleGeometricObject.java
@@ -2,4 +2,8 @@ public class SimpleGeometricObject {
     private String color = "white";
     private boolean filled;
     private java.util.Date dateCreated;
+
+    public SimpleGeometricObject() {
+        dateCreated = new java.util.Date();
+    }
 }
