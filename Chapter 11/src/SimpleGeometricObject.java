f7771d8c183b08e2e1717ad8d2ffa2b5b3eb2061
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-26 22:24:33 +0600

    Add getter() and setter() for filled

--- Chapter 11/src/SimpleGeometricObject.java
+++ Chapter 11/src/SimpleGeometricObject.java
@@ -19,4 +19,12 @@ public class SimpleGeometricObject {
     public void setColor(String color) {
         this.color = color;
     }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
 }
