5e5cf04c8a3bd5ec34db355ba926c8419a54eb05
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-27 23:02:02 +0600

    Modify the constructor with no parameters

--- Chapter 11/src/RectangleFromSimpleGeometricObject.java
+++ Chapter 11/src/RectangleFromSimpleGeometricObject.java
@@ -2,8 +2,9 @@ public class RectangleFromSimpleGeometricObject extends SimpleGeometricObject {
     private double width;
     private double height;
 
-    public RectangleFromSimpleGeometricObject(){
-
+    public RectangleFromSimpleGeometricObject(double width, double height){
+        this.width = width;
+        this.height = height;
     }
 
     public RectangleFromSimpleGeometricObject(double width, double height, String color, boolean filled) {
