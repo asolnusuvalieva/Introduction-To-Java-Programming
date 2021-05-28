a39bfb41d07c51c209a1edfe5a0c4780ebab11d9
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-28 23:37:45 +0600

    Add cities

--- Chapter 11/src/TestArrayList.java
+++ Chapter 11/src/TestArrayList.java
@@ -4,5 +4,18 @@ public class TestArrayList {
     public static void main(String[] args) {
         ArrayList<String> cityList = new ArrayList<>();
 
+        // Add some cities in the list
+        cityList.add("London");
+        // cityList now contains [London]
+        cityList.add("Denver");
+        // cityList now contains [London, Denver]
+        cityList.add("Paris");
+        // cityList now contains [London, Denver, Paris]
+        cityList.add("Miami");
+        // cityList now contains [London, Denver, Paris, Miami
+        cityList.add("Seoul");
+        // Contains [London, Denver, Paris, Miami, Seoul]
+        cityList.add("Tokyo");
+        // Contains [London, Denver, Paris, Miami, Seoul, Tokyo]
     }
 }
