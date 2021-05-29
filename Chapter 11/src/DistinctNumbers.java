eee1de30b7da24222be1d27d6f73c79e4016fe00
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-29 22:58:51 +0600

    Add output info

--- Chapter 11/src/DistinctNumbers.java
+++ Chapter 11/src/DistinctNumbers.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DistinctNumbers {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter integers (input ends with 0): ");
     }
 }
