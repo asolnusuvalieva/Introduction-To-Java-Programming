9e65daaa019c03c2a3441021d36e3518f8fa65d4
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-29 23:00:45 +0600

    Add the loop that initializes

--- Chapter 11/src/DistinctNumbers.java
+++ Chapter 11/src/DistinctNumbers.java
@@ -6,5 +6,13 @@ public class DistinctNumbers {
         ArrayList<Integer> list = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         System.out.print("Enter integers (input ends with 0): ");
+
+        int value;
+        do{
+            value = input.nextInt();
+
+            if (!list.contains(value) && value != 0)
+                list.add(value);
+        }while(value != 0);
     }
 }
