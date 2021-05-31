759dc2bd94a430aae4c4d61caddecb10b514a2bc
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-31 23:00:39 +0600

    Add if construct

--- Chapter 12/src/QuotientWithIf.java
+++ Chapter 12/src/QuotientWithIf.java
@@ -9,5 +9,10 @@ public class QuotientWithIf {
 
         int number1 = input.nextInt();
         int number2 = input.nextInt();
+
+        if (number2 != 0) System.out.println(number1 + " / " + number2
+                + " is " + (number1 / number2));
+        else
+            System.out.println("Divisor cannot be zero ");
     }
 }
