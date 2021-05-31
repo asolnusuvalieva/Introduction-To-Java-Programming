ad917e83503afca57f1960f39d22f73b844e2e3a
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-31 22:54:43 +0600

    Add user output info

--- Chapter 12/src/Quotient.java
+++ Chapter 12/src/Quotient.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Quotient {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Prompt the user to enter two integers
+        System.out.print("Enter two integers: ");
     }
 }
