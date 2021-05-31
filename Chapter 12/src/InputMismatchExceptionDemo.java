88efb2afa053d218da3dc72eb7b61d0947c94814
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-05-31 23:01:54 +0600

    Add data fields

--- Chapter 12/src/InputMismatchExceptionDemo.java
+++ Chapter 12/src/InputMismatchExceptionDemo.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class InputMismatchExceptionDemo {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        boolean continueInput = true;
     }
 }
