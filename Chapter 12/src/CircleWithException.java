2b4e57dcb654b9d48e7a51532a8c58a2bdbc4c2e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-02 21:52:32 +0600

    Add setter for radius

--- Chapter 12/src/CircleWithException.java
+++ Chapter 12/src/CircleWithException.java
@@ -2,4 +2,11 @@ public class CircleWithException {
     private double radius;
     private static int numberOfObjects = 0;
 
+    public CircleWithException(double newRadius) {
+
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
