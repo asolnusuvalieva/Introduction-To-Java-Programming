4b830704c320b55409f1d42b3bad18e8baa53442
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-04 23:11:54 +0600

    Check if source file exists

--- Chapter 12/src/ReplaceText.java
+++ Chapter 12/src/ReplaceText.java
@@ -1,9 +1,18 @@
+import java.io.File;
+
 public class ReplaceText {
     public static void main(String[] args) {
         //Check command line parameter usage
         if(args.length != 4){
             System.out.println("Usage: java ReplaceText sourceFile targetFile oldStr newStr");
             System.exit(1);
+        }
+
+        // Check if source file exists
+        File sourceFile = new File(args[0]);
+        if(!sourceFile.exists()){
+            System.out.println("Source file " + args[0] + " doesn't exist");
+            System.exit(2);
         }
     }
 }
