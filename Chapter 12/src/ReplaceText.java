0a2a63b946af78841a2e7aa4be7637663a0c6807
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-04 23:11:33 +0600

    Check command line parameter usage

--- Chapter 12/src/ReplaceText.java
+++ Chapter 12/src/ReplaceText.java
@@ -1,5 +1,9 @@
 public class ReplaceText {
     public static void main(String[] args) {
-
+        //Check command line parameter usage
+        if(args.length != 4){
+            System.out.println("Usage: java ReplaceText sourceFile targetFile oldStr newStr");
+            System.exit(1);
+        }
     }
 }
