f1e5463e0bc75928498c0e22ac0729f72ff43fce
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-04 23:17:13 +0600

    Finish the listing

--- Chapter 12/src/ReplaceText.java
+++ Chapter 12/src/ReplaceText.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class ReplaceText {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         //Check command line parameter usage
         if(args.length != 4){
             System.out.println("Usage: java ReplaceText sourceFile targetFile oldStr newStr");
