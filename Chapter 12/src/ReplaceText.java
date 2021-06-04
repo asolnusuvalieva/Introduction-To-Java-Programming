46f70b62660673d5b422ee57ee807a4e101fb4e7
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-04 23:16:12 +0600

    Create input and output files

--- Chapter 12/src/ReplaceText.java
+++ Chapter 12/src/ReplaceText.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class ReplaceText {
     public static void main(String[] args) {
@@ -20,6 +22,18 @@ public class ReplaceText {
         if(targetFile.exists()){
             System.out.println("Target file " + args[1] + " already exists");
             System.exit(3);
+        }
+
+        try(
+                // Create input and output files
+                Scanner input = new Scanner(sourceFile);
+                PrintWriter output = new PrintWriter(targetFile);
+        ){
+            while (input.hasNext()){
+                String s1 = input.nextLine();
+                String s2 = s1.replaceAll(args[2], args[3]);
+                output.println(s2);
+            }
         }
     }
 }
