d53c509e9862d7f42a5fb90d10b73d6d5e0b7d10
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-05 23:07:47 +0600

    Check the length

--- Chapter 12/src/Problem05.java
+++ Chapter 12/src/Problem05.java
@@ -1,5 +1,8 @@
 public class Problem05 {
     public static void main(String[] args) {
-
+        if(args.length != 2){
+            System.err.println("Usage: java Problem05 <Text To Remove> <File To Process>");
+            System.exit(-1); //unsuccessful program completion (any other number than 0)
+        }
     }
 }
