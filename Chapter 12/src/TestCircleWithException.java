3b96fee73dfca3f4ab3dd85ee9d24a927dce383b
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-05 23:05:43 +0600

    Create 3 objects

--- Chapter 12/src/TestCircleWithException.java
+++ Chapter 12/src/TestCircleWithException.java
@@ -1,7 +1,9 @@
 public class TestCircleWithException {
     public static void main(String[] args) {
         try{
-
+            CircleWithException c1 = new CircleWithException(5);
+            CircleWithException c2 = new CircleWithException(-5);
+            CircleWithException c3 = new CircleWithException(0);
         }catch (IllegalArgumentException ex){
 
         }
