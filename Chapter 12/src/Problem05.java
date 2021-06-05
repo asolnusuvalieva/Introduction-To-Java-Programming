ec0d15b43860a133d517964854fb04a3bb1b0461
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-05 23:08:14 +0600

    Create a file

--- Chapter 12/src/Problem05.java
+++ Chapter 12/src/Problem05.java
@@ -1,8 +1,16 @@
+import java.io.File;
+
 public class Problem05 {
     public static void main(String[] args) {
         if(args.length != 2){
             System.err.println("Usage: java Problem05 <Text To Remove> <File To Process>");
             System.exit(-1); //unsuccessful program completion (any other number than 0)
+        }
+
+        File fileToProcess = new File(args[1]);
+        if(!fileToProcess.exists()){
+            System.out.printf("File '%s' doesn't exist.%n", args[1]);
+            System.exit(-1);
         }
     }
 }
