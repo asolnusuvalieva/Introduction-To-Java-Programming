9b5d0951dd8f601b097ffae1142921a3196bea5a
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-07 21:51:10 +0600

    Add numbers

--- Chapter 13/src/LargestNumbers.java
+++ Chapter 13/src/LargestNumbers.java
@@ -3,5 +3,8 @@ import java.util.ArrayList;
 public class LargestNumbers {
     public static void main(String[] args) {
         ArrayList<Number> list = new ArrayList<>();
+
+        list.add(45); // Add an integer
+        list.add(3445.53); // Add a double
     }
 }
