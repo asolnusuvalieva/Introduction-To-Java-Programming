6e959e2aceef67253b8cad2c6cebe63af152b8ce
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-07 21:46:24 +0600

    Finish Listing 12.17

--- Chapter 12/src/ReadFileFromURL.java
+++ Chapter 12/src/ReadFileFromURL.java
@@ -7,9 +7,17 @@ public class ReadFileFromURL {
 
         try{
             java.net.URL url = new java.net.URL(URLString);
-
+            int count = 0;
+            Scanner input = new Scanner(url.openStream());
+            while (input.hasNext()) {
+                String line = input.nextLine();
+                count += line.length();
+            }
+            System.out.println("The file size is " + count + " characters");
         }catch(java.net.MalformedURLException ex){
-
+            System.out.println("Invalid URL");
+        }catch (java.io.IOException ex){
+            System.out.println("I/O Errors: no such file");
         }
     }
 }
