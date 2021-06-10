53514f13ef2e95aa32ff0d62b46c3577be48fd84
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-10 23:17:56 +0600

    Add display info

--- Chapter 13/src/TestCalendar.java
+++ Chapter 13/src/TestCalendar.java
@@ -1,8 +1,11 @@
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class TestCalendar {
     public static void main(String[] args) {
         Calendar calendar = new GregorianCalendar();
+
+        System.out.println("Current time is " + new Date());
     }
 }
