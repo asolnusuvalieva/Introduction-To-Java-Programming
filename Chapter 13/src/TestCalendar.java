a9f6c5461116ce6bf2616324faa8fe50e84a78e5
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-10 23:17:02 +0600

    Create a calendar

--- Chapter 13/src/TestCalendar.java
+++ Chapter 13/src/TestCalendar.java
@@ -1,5 +1,8 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class TestCalendar {
     public static void main(String[] args) {
-
+        Calendar calendar = new GregorianCalendar();
     }
 }
