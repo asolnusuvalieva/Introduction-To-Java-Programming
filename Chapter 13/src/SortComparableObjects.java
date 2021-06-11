19d412daef7f3fdd5e9ed25575b049ef1f91aa1e
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-11 23:40:26 +0600

    Add the array

--- Chapter 13/src/SortComparableObjects.java
+++ Chapter 13/src/SortComparableObjects.java
@@ -1,5 +1,6 @@
 public class SortComparableObjects {
     public static void main(String[] args) {
-
+        String[] cities = {"Savannah", "Boston", "Atlanta", "Tampa"};
+        java.util.Arrays.sort(cities);
     }
 }
