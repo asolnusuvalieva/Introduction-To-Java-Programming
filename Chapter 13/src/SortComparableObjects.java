98572af08dec8515f599dce6d863b71aed9bbd6d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-11 23:40:55 +0600

    Add the for loop

--- Chapter 13/src/SortComparableObjects.java
+++ Chapter 13/src/SortComparableObjects.java
@@ -2,5 +2,8 @@ public class SortComparableObjects {
     public static void main(String[] args) {
         String[] cities = {"Savannah", "Boston", "Atlanta", "Tampa"};
         java.util.Arrays.sort(cities);
+        for (String city: cities)
+            System.out.print(city + " ");
+        System.out.println();
     }
 }
