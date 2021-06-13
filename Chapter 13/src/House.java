08451ad220eb943e0a52e847fff4ce82d16df9f0
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-13 22:28:00 +0600

    Add a constructor

--- Chapter 13/src/House.java
+++ Chapter 13/src/House.java
@@ -2,4 +2,10 @@ public class House {
     private int id;
     private double area;
     private java.util.Date whenBuilt;
+
+    public House(int id, double area) {
+        this.id = id;
+        this.area = area;
+        whenBuilt = new java.util.Date();
+    }
 }
