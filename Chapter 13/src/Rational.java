ccb69c22ae04b3509d3f7258ff8435ebb487dbee
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-15 21:34:01 +0600

    Add gcd() method

--- Chapter 13/src/Rational.java
+++ Chapter 13/src/Rational.java
@@ -1,4 +1,11 @@
-public class Rational{
+public class Rational {
     private long numerator = 0;
     private long denominator = 1;
+
+    private static long gcd(long n, long d) {
+        long n1 = Math.abs(n);
+        long n2 = Math.abs(d);
+        int gcd = 1;
+        return gcd;
+    }
 }
