ab605cddf47369250b7e0501229738e2990d2fdd
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-15 21:35:26 +0600

    Add a constructor

--- Chapter 13/src/Rational.java
+++ Chapter 13/src/Rational.java
@@ -2,6 +2,12 @@ public class Rational {
     private long numerator = 0;
     private long denominator = 1;
 
+    public Rational(long numerator, long denominator) {
+        long gcd = gcd(numerator, denominator);
+        this.numerator = ((denominator > 0) ? 1 : -1) * numerator / gcd;
+        this.denominator = Math.abs(denominator) / gcd;
+    }
+
     private static long gcd(long n, long d) {
         long n1 = Math.abs(n);
         long n2 = Math.abs(d);
