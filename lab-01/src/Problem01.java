cc0aa0b72b55cfb1b4a9e1a97b690ead442d162a
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-24 23:52:45 +0600

    Add data fields

---
+++ lab-01/src/Problem01.java
@@ -0,0 +1,11 @@
+public class Problem01 {
+    final static int WIDTH = 20;
+    final static int HEIGHT = 20;
+
+    final static char FIELD_TURTLE_CELL = 'T';
+    final static char FIELD_EMPTY_CELL = '.';
+    final static char FIELD_MARKED_CELL = '*';
+
+    static char[][] fieldContent;
+}
+
