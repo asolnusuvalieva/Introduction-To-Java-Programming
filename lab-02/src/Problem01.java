1b1be2daa20ac6aa8f91cc3c66d64e3704ea4ebc
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-26 23:05:21 +0600

    Add local vars

--- lab-02/src/Problem01.java
+++ lab-02/src/Problem01.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Problem01 {
     public static void main(String[] args) {
+        Field field = new Field();
+        Turtle turtle = new Turtle(field);
 
+        Scanner scanner = new Scanner(System.in);
+
+        String input;
     }
 }
