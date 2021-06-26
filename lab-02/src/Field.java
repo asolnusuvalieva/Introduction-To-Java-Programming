b4471fa43e9caf0c4dbfe9626464b90332a06fd2
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-06-26 22:57:00 +0600

    Add properties

---
+++ lab-02/src/Field.java
@@ -0,0 +1,10 @@
+public class Field {
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 20;
+
+    private static final char TURTLE_CELL = 'T';
+    private static final char EMPTY_CELL = '.';
+    private static final char MARKED_CELL = '*';
+
+    private char[][] content;
+}
