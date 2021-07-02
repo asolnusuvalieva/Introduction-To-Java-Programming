be483f5d03bb85b29d7f664c94395fc7a3d68136
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-02 23:29:18 +0600

    Add addZerosBeforeEvenValues() method

--- lab-04/src/Problem01.java
+++ lab-04/src/Problem01.java
@@ -1,11 +1,15 @@
 import java.util.ArrayList;
 
 public class Problem01 {
-    private static void reverse(ArrayList<Integer> numbers){
-        for(int i = 0, j = numbers.size() - 1; i < j; i++, j--){
+    private static void reverse(ArrayList<Integer> numbers) {
+        for (int i = 0, j = numbers.size() - 1; i < j; i++, j--) {
             int temp = numbers.get(i); //reading an information, automatic boxing
             numbers.set(i, numbers.get(j)); //writing/replacing
             numbers.set(j, temp);
         }
+    }
+
+    private static void addZerosBeforeEvenValues(ArrayList<Integer> numbers) {
+
     }
 }
