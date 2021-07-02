084f28b90833197d40872e71dda77f1b0f52bc6b
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-02 23:32:52 +0600

    Add user output info

--- lab-04/src/Problem01.java
+++ lab-04/src/Problem01.java
@@ -22,6 +22,10 @@ public class Problem01 {
         addZerosBeforeEvenValues(numbers);
         System.out.println("After insertion:");
         System.out.println(numbers); //toString()
+
+        removeOddValues(numbers);
+        System.out.println("After deletions:");
+        System.out.println(numbers); //toString()
     }
     private static void reverse(ArrayList<Integer> numbers) {
         for (int i = 0, j = numbers.size() - 1; i < j; i++, j--) {
