68400ea77f56742de65d83e423a6465f727792ad
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-02 23:30:52 +0600

    Add the main method

--- lab-04/src/Problem01.java
+++ lab-04/src/Problem01.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class Problem01 {
+    public static void main(String[] args) {
+
+    }
     private static void reverse(ArrayList<Integer> numbers) {
         for (int i = 0, j = numbers.size() - 1; i < j; i++, j--) {
             int temp = numbers.get(i); //reading an information, automatic boxing
