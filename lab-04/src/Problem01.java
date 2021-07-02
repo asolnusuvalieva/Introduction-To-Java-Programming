f7fb6237f6055978546b9d1de29b7becb70c63c3
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-02 23:28:45 +0600

    Implement reverse() method

--- lab-04/src/Problem01.java
+++ lab-04/src/Problem01.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 public class Problem01 {
     private static void reverse(ArrayList<Integer> numbers){
-
+        for(int i = 0, j = numbers.size() - 1; i < j; i++, j--){
+            int temp = numbers.get(i); //reading an information, automatic boxing
+            numbers.set(i, numbers.get(j)); //writing/replacing
+            numbers.set(j, temp);
+        }
     }
 }
