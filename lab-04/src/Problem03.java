31b1d92e2890bafcbcd8d1fc8306312eff6f82d7
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-03 22:46:25 +0600

    Implement addZerosBeforeEvenValues() method

--- lab-04/src/Problem03.java
+++ lab-04/src/Problem03.java
@@ -8,6 +8,12 @@ public class Problem03 {
     }
 
     private static void addZerosBeforeEvenValues(ArrayListInt numbers){
-
+        //inserts value 0 before each even number
+        for(int i = 0; i < numbers.size(); i++){
+            if(numbers.get(i) % 2 == 0){
+                numbers.add(i, 0);//inserting a new value
+                i++; //to skip the element we just analyzed
+            }
+        }
     }
 }
