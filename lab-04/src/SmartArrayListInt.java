c00112c763cf2ca01afe40812fd27bc089d2ca5d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-04 22:14:04 +0600

    Add data fields

--- lab-04/src/SmartArrayListInt.java
+++ lab-04/src/SmartArrayListInt.java
@@ -1,4 +1,7 @@
 public class SmartArrayListInt {
     private static final int DEFAULT_CAPACITY = 8;
     private static final int SIZE_MULTIPLIER = 2;
+
+    private int[] data;
+    private int size; //actual number of existing elements
 }
