4de334be53dcfb655d06fda838bf94f8b1e86856
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-09 23:34:02 +0600

    Add user output info

--- lab-04/src/Problem04.java
+++ lab-04/src/Problem04.java
@@ -13,6 +13,9 @@ public class Problem04 {
         System.out.println("Before reversing:");
         System.out.println(numbers); //toString()
 
+        System.out.println("After reversing:");
+        reverse(numbers);
+        System.out.println(numbers); //toString()
     }
     private static void reverse(SmartArrayListInt numbers){
         for(int i = 0, j = numbers.size() - 1; i < j; i++, j--){
