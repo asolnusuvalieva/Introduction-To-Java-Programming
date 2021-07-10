f20be4ef5580f6137e664c4ca282e03095804693
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-10 23:03:57 +0600

    Add data fields

--- lab-04/src/Problem05.java
+++ lab-04/src/Problem05.java
@@ -1,5 +1,8 @@
 public class Problem05 {
     public static void main(String[] args) {
+        final int COUNT = 10000;
 
+        long startTime, endTime, deltaTime;
+        startTime = System.nanoTime();
     }
 }
