c0a3945c9c364336e694b6f7fc13fefcb6919e3f
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-10 23:04:34 +0600

    Add an array

--- lab-04/src/Problem05.java
+++ lab-04/src/Problem05.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class Problem05 {
     public static void main(String[] args) {
         final int COUNT = 10000;
 
         long startTime, endTime, deltaTime;
         startTime = System.nanoTime();
+
+        {
+            ArrayList<Integer> list = new ArrayList<>();
+        }
     }
 }
