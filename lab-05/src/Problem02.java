d9f3b13ab14819bce10aabffd2fad3327cf51715
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-12 22:49:35 +0600

    Configure the window

--- lab-05/src/Problem02.java
+++ lab-05/src/Problem02.java
@@ -1,8 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Problem02 extends JFrame {
     JPanel mainPanel;
     Problem02(){
-
+        /* WINDOW */
+        setSize(400, 400);
+        setLocationRelativeTo(null); //set the window to the center of the screen
+        setTitle("Lab-05: Problem02");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //What to do by default when we close the window? //it'll stop the program
+        setLayout(new BorderLayout()); //Choose a layout engine
     }
 }
