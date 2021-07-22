129f6cb75d12c109e86fac315907876cf671681c
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-22 23:49:47 +0600

    Implement a method

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
@@ -14,5 +15,7 @@ public class Problem03 {
         public void mouseMoved(MouseEvent e) {}
         public void mouseDragged(MouseEvent e){}
 
+        //Methods from KeyListener
+        public void keyPressed(KeyEvent e){}
     }
 }
