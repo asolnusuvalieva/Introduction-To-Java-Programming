35726118f465f0a7f49b3be74c313aa49f3d2f5d
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-22 23:47:27 +0600

    Create an abstract class

---
+++ lab-05/src/Problem03.java
@@ -0,0 +1,8 @@
+import java.awt.event.KeyListener;
+import java.awt.event.MouseMotionListener;
+
+public class Problem03 {
+    abstract class Reporter implements MouseMotionListener, KeyListener {
+
+    }
+}
