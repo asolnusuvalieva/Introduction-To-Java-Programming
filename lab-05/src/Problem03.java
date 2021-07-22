1acb93a5e1ead782c824310045fafb2faee4ad54
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-22 23:48:24 +0600

    Add data fields

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -1,8 +1,10 @@
+import javax.swing.*;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseMotionListener;
 
 public class Problem03 {
     abstract class Reporter implements MouseMotionListener, KeyListener {
+        protected final JFrame window; //window is repeated in two different listeners
 
     }
 }
