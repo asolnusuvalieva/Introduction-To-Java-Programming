47988e4d6c4188104f63e0918bdc034594bff2fa
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-23 23:48:25 +0600

    Move the pieces of code

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -4,20 +4,22 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-public class Problem03 {
-    abstract class Reporter implements MouseMotionListener, KeyListener {
-        protected final JFrame window; //window is repeated in two different listeners
-        Reporter(JFrame window){
-            this.window = window;
-        }
-
-        //Methods from MouseMotionListener
-        public void mouseMoved(MouseEvent e) {}
-        public void mouseDragged(MouseEvent e){}
-
-        //Methods from KeyListener
-        public void keyPressed(KeyEvent e){}
-        public void keyTyped(KeyEvent e){}
-        public void keyReleased(KeyEvent e){}
+abstract class Reporter implements MouseMotionListener, KeyListener {
+    protected final JFrame window; //window is repeated in two different listeners
+    Reporter(JFrame window){
+        this.window = window;
     }
+
+    //Methods from MouseMotionListener
+    public void mouseMoved(MouseEvent e) {}
+    public void mouseDragged(MouseEvent e){}
+
+    //Methods from KeyListener
+    public void keyPressed(KeyEvent e){}
+    public void keyTyped(KeyEvent e){}
+    public void keyReleased(KeyEvent e){}
+}
+
+public class Problem03 {
+
 }
