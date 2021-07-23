582c2f47e31d37c0a3aff2a96f6e8b2cefd11419
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-23 23:53:15 +0600

    Configure the window

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -39,6 +40,11 @@ class KeyReporter extends Reporter {
 }
 public class Problem03 extends JFrame {
     Problem03(){
-
+        /* WINDOW */
+        setSize(400, 400);
+        setLocationRelativeTo(null); //set the window to the center of the screen
+        setTitle("Lab-05: Problem03");
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //What to do by default when we close the window? //it'll stop the program
+        setLayout(new BorderLayout()); //Choose a layout engine
     }
 }
