9cee84bffc0ce06b0541613ef15a4f8d571e527b
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-24 23:57:08 +0600

    Implement the window

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -32,6 +32,10 @@ class Checkerboard extends JPanel {
 }
 public class Problem04 extends JFrame{
     Problem04(){
-
+        setSize(400, 400);
+        setLocationRelativeTo(null); //set the window to the center of the screen
+        setTitle("Lab-05: Problem04");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);//What to do by default when we close the window? //it'll stop the program
+        setLayout(new BorderLayout()); //Choose a layout engine
     }
 }
