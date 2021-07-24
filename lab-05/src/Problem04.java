c742286e66e61c367d51fae85baffe3e54d433f8
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-24 23:55:00 +0600

    Override a method

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 class Checkerboard extends JPanel {
     private static final int FIELD_WIDTH = 10;
     private static final int FIELD_HEIGHT = 10;
+
+    protected void paintComponent(Graphics g) {
+
+    }
 }
 public class Problem04 {
 }
