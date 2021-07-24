895f5e5fdef4ba9768239203a370026d6433ef3a
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-24 23:54:30 +0600

    Add data fields

--- lab-05/src/Problem04.java
+++ lab-05/src/Problem04.java
@@ -1,7 +1,8 @@
 import javax.swing.*;
 
 class Checkerboard extends JPanel {
-
+    private static final int FIELD_WIDTH = 10;
+    private static final int FIELD_HEIGHT = 10;
 }
 public class Problem04 {
 }
