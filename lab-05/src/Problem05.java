4ad6f5f30a831483ada0d2b87eeebb0ee1a4c9c0
asolnusuvalieva <asolnusuvalieva@gmail.com>
2021-07-26 23:52:28 +0600

    Create a constructor

--- lab-05/src/Problem05.java
+++ lab-05/src/Problem05.java
@@ -3,6 +3,10 @@ class Field{
     private static final int DEFAULT_FIELD_HEIGHT = 10;
 
     private final int width, height;
+    Field(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
 }
 public class Problem05 {
 }
